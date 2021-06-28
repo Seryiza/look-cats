507f05a69d14544fb7e53560cbae7624cfbc28c6
Sergey Zaborovsky <seryiza@yandex.ru>
2021-06-28 21:45:08 +1100

    Add Tailwind

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default () => {
-  return <h1>Wow!</h1>
+  return (
+    <h1 className='text-6xl font-extrabold text-gray-900'>
+      <span>Hello, </span>
+      <span className='text-indigo-600'>World!</span>
+    </h1>
+  )
 }
