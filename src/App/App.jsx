512cab0ad94688a38561e376b4e8a7df1d03354a
Sergey Zaborovsky <seryiza@yandex.ru>
2021-07-08 20:34:26 +1100

    Fix paddings near top bar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,7 +6,7 @@ import Footer from '../Footer'
 
 export default () => {
   return (
-    <div className='container mx-auto p-8'>
+    <div className='container mx-auto p-4'>
       <TopBar />
       <Feed />
       <Footer />
--- src/TopBar/index.jsx
+++ src/TopBar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <div className='flex content-center my-4 p-4 border-2 border-yellow-600'>
+    <div className='flex content-center mb-4 p-4 border-2 border-yellow-600'>
       <h2 className='flex-1 text-xl tracking-wider text-yellow-600 font-bold'>
         Look, Cats!
       </h2>
